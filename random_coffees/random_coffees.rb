8714a30c4fd1b4e207c59592f1ebcad2da2ab2c1
Lawrence Archer <lawrence.archer@gmail.com>
2018-08-15 07:39:33 +0100

    simplified to weekly runnable script

--- random_coffees/random_coffees.rb
+++ random_coffees/random_coffees.rb
@@ -1,9 +1,9 @@
 require "mail"
-require "erb"
 
 def coffee_selector
-	arr = ["calvin", "archer", "phil", "vera", "ade", "calum", "daniel", "jade", "jade G", "jasmine", "kris", "marc", "matt", "mike", "rhys", "roxy", "steph", "tom", "vasyl"]
+	arr = ["@archer", "@ade", "@calum", "@calvin", "@daniel", "@jade", "@jade G", "@jasmine", "@kris", "@marc", "@matt", "@mike", "@phil", "@rhys", "@roxy", "@steph", "@tom", "@vasyl", "@vera"]
 	res_arr = []
+	res_str = "This weeks' random coffees:\n"
 	while arr.length > 0 do
 		if arr.length == 1
 			res_arr[-1].push(arr[0])
@@ -16,34 +16,15 @@ def coffee_selector
 		arr.delete(name2)
 		res_arr.push([name1, name2])
 	end
-	res_arr
-end
 
-res_arr = coffee_selector
-
-res_arr.each do |x|
-	if x.length == 3
-		puts "#{x[0]} must meet #{x[1]} and #{x[2]} for a beverage at some point this week"
-	else
-		puts "#{x[0]} must meet #{x[1]} for a beverage at some point this week"
+	res_arr.each do |x|
+		if x.length == 3
+			res_str << "#{x[0]} - #{x[1]} and #{x[2]}\n"
+		else
+			res_str << "#{x[0]} - #{x[1]}\n"
+		end
 	end
+	puts res_str
 end
 
-template_email = File.read "email.html.erb"
-erb_template = ERB.new template_email
-
-
-
-
-
-
-
-
-mail = Mail.new do
-  from    'lawrence@veeqo.com'
-  to      'lawrence.archer@gmail.com'
-  subject 'This is a test email'
-  body    File.read('body.txt')
-end
-
-mail.deliver!
+coffee_selector
